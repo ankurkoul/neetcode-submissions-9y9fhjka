8e124a13bad6c7a93f8f1c884dd0d9b03ff6f90a
ANKUR KOUL <er.ankurkoul@gmail.com>
2026-05-05 08:22:41 +0530

    Add: max-area-of-island - submission-4

---
+++ Data Structures & Algorithms/max-area-of-island/submission-4.java
@@ -0,0 +1,52 @@
+class Solution {
+    int n,m,countIslands;
+    public int maxAreaOfIsland(int[][] grid) {
+       //ICore
+
+     n=grid.length;
+     m=grid[0].length;
+     countIslands=0;
+     int max=0;
+
+     /**
+    
+        👉 “Number of islands → count DFS calls”
+        👉 “Max area → each DFS returns a value”
+     */
+     for(int r=0;r<n;r++){
+        for(int c=0;c<m;c++){
+            //we need area  and among them max area ,
+            // area is count of islands in that connected path
+            if(grid[r][c]==1){
+               max=Math.max(max , dfs(r,c,grid));
+            }
+             
+        }
+     }
+
+     return max;
+    }
+
+    int[][] dirs= {{0,1},{1,0},{0,-1},{-1,0}};
+
+    private void dfs_countAllIslands(int r, int c, int[][] grid){
+        if(r<0 || c<0 || r==n || c==m || grid[r][c]==0)return;
+        //mark visited by sink
+        grid[r][c]=0;
+        countIslands++;
+        for(int[] d: dirs){
+            dfs_countAllIslands(r+d[0],c+d[1],grid);
+        }
+
+    }
+
+     private int dfs(int r, int c, int[][] grid){
+         if(r<0 || c<0 || r==n || c==m || grid[r][c]==0)return 0;
+         grid[r][c]=0;
+         int count=1;
+         for(int[] d: dirs){
+           count+= dfs(r+d[0],c+d[1],grid);
+            }
+        return count;
+     }
+}
